1754f0d7679b1e0a7301944e9a80c87abbf55c46
Giulliano Macedo <giulliano.macedo@walmart.com>
2017-12-16 21:32:51 -0200

    fix search when on searchPage

--- src/js/Header.jsx
+++ src/js/Header.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { Link, Redirect } from "react-router-dom";
+import PropTypes from "prop-types";
+import { Link, Redirect } from 'react-router-dom';
 
 
 class Header extends Component {
   state = {
-    searchTerm: "",
+    searchTerm: '',
     redirect: false
   };
   handleSearchTermChange = event => {
@@ -16,28 +17,45 @@ class Header extends Component {
     }
   };
   render() {
+    let searchButton = <button onClick={() => {
+          this.handleSearchSubmit(this.state.searchTerm);
+        }}>
+        Go
+      </button>;
+    if(this.props.searchPage) {
+      searchButton = <button onClick={() => {
+            this.props.handleSearchResult(this.state.searchTerm);
+          }}>
+          Go
+        </button>;
+    }
     const { redirect } = this.state;
     if (redirect) {
-      return <Redirect to={{ pathname: "/items", search: `?search=${this.state.searchTerm}` }} />;
+      return <Redirect to={{ pathname: '/items', search: `?search=${this.state.searchTerm}` }} />;
     }
-    return (
-      <header>
+    return <header>
         <div>
           <h1>
-            <Link to="/">Busca</Link>
+            <Link to="/">Home</Link>
           </h1>
-          <input onChange={this.handleSearchTermChange} type="search" placeholder="Nunca dejes de buscar" />
-          <button
-            onClick={() => {
-              this.handleSearchSubmit(this.state.searchTerm);
-            }}
-          >
-            Go
-          </button>
+          <input onChange={this.handleSearchTermChange} type="search" placeholder={this.props.searchTerm} />
+          {searchButton}
         </div>
-      </header>
-    );
+      </header>;
   }
 }
+
+Header.propTypes = {
+  searchTerm: PropTypes.string,
+  handleSearchResult: PropTypes.func,
+  searchPage: PropTypes.bool
+};
+
+Header.defaultProps = {
+  searchPage: false,
+  handleSearchResult: function noop() {},
+  searchTerm: "Nunca dejas de comprar"
+};
+
 
 export default Header
--- src/js/Search.jsx
+++ src/js/Search.jsx
@@ -6,32 +6,37 @@ import ShelfCard from "./ProdutCard";
 
 class Search extends Component {
   state = {
-    results: []
-    // searchTerm: '',
+    results: [],
+    searchTerm: ""
   };
   componentDidMount() {
     const regex = /=(.+)/;
     const paramsSearch = this.props.location.search.match(regex);
     const searchTerm = paramsSearch[1];
-
+    this.handleSearchResult(searchTerm);
+  }
+  handleSearchResult = (searchTerm) => {
     const url = `http://localhost:3000/api/items?q='${searchTerm}`;
     axios
       .get(url)
       .then(response => {
         const data = { results: response.data.items };
         this.setState(data);
+        this.setState({ searchTerm }); // eslint-disable-line
       })
       .catch(error => console.log(error));
-  }
-
+  };
   render() {
-    return <div className="search">
-        Busca
-        <Header />
-        <div className="wrapper">
-          {this.state.results.map(product => <ShelfCard key={product.id} {...product} />)}
-        </div>
-      </div>;
+    let searchBar;
+    if (this.state.searchTerm.length > 0) {
+      searchBar = <Header searchPage handleSearchResult={this.handleSearchResult} searchTerm={this.state.searchTerm} />;
+    }
+    return (
+      <div className="search">
+        {searchBar}
+        <div className="wrapper">{this.state.results.map(product => <ShelfCard key={product.id} {...product} />)}</div>
+      </div>
+    );
   }
 }
 
