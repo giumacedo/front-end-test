678c8b14516244e6bc4c52ade0d230937a7d6a56
Giulliano Macedo <giulliano.macedo@walmart.com>
2017-12-16 23:20:04 -0200

    add detail page

--- src/js/Details.jsx
+++ src/js/Details.jsx
@@ -1,11 +1,61 @@
-import React from 'react';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
 import Header from './Header';
 
-const Details = () => (
-  <div className='details'>
-    Detalhe
-    <Header />
-  </div>
-);
+class Details extends Component {
+  state = {
+    apiData: {}
+  };
+  componentDidMount() {
+    const paramsProductId = this.props.match.params.id;
+    axios
+      .get(`http://localhost:3000/api/item/${paramsProductId}`)
+      .then((response: { data: { rating: string } }) => {
+        this.setState({ apiData: response.data.item });
+      });
+  }
+  render() {
+    const { picture, condition, title, description } = this.state.apiData;
+    let priceCurrency;
+    let totalPrice;
+    let soldQuantity;
+    if (this.state.apiData.price) {
+    priceCurrency = this.state.apiData.price.currency;
+    totalPrice = this.state.apiData.price.price;
+    soldQuantity = this.state.apiData.sold_quantity;
+    }
+    return <div className="details">
+        <Header />
+        <div className="layout-col-left">
+          <figure>
+            <img alt={picture} src={picture} />
+          </figure>
+        </div>
+        <div className="layout-col-right">
+          <section>
+            <div>
+              {condition} - {soldQuantity} vendidos
+            </div>
+            <header>
+              <h1>{title}</h1>
+            </header>
+            <fieldset>
+              <span>
+                <span>{priceCurrency}</span>
+                <span>{totalPrice}</span>
+              </span>
+            </fieldset>
+          </section>
+          <section>{description}</section>
+        </div>
+      </div>;
+  }
+}
+
+Details.propTypes = {
+  match: PropTypes.object // eslint-disable-line
+};
+
 
 export default Details;
